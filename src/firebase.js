98632eea6733cf22552924cf6d838a1e6a85cea5
Harish Babu <68015077+harishbabu2007@users.noreply.github.com>
2021-01-26 17:38:52 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVgQrjv2a8XhE-ji638gi-1uAMKjtl2Lo",
-  authDomain: "web-chat-bb872.firebaseapp.com",
-  projectId: "web-chat-bb872",
-  storageBucket: "web-chat-bb872.appspot.com",
-  messagingSenderId: "345229271155",
-  appId: "1:345229271155:web:de94b899a2f7426cdf4a06",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = firebase.initializeApp(firebaseConfig);
